9cd6ed62bf5282f87629be25da50e23b7ee2a0d8
faizal007-dev <faizal@growwstacks.com>
2026-03-09 16:43:05 +0530

    google tag update

--- analytics-loader.js
+++ analytics-loader.js
@@ -5,8 +5,14 @@
   y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
 })(window, document, "clarity", "script", "vk6xy7fz99");
 
+// Google Tag Manager
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NS74XWD2');
 
-// Google Ads Global Tag
+// Google Ads Conversion Tracking
 var gtagScript = document.createElement("script");
 gtagScript.async = true;
 gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=AW-17936939840";
