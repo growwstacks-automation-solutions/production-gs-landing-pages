a36b2baeab78ac8275dd90c62f069bf8239389e4
shubhampatel1010 <shubhampatel@growwstacks.com>
2026-03-23 15:50:55 +0530

    create file via API

--- workflows-make.com/workflows-make.com.js
+++ workflows-make.com/workflows-make.com.js
@@ -1,4 +1,6 @@
 var ALL_WORKFLOWS = [
+{u:'https://www.growwstacks.com/how-to-guides/2612-create-engagements-in-hubspot-crm-from-finished-outgoing-yodel-io-calls',i:'https://ik.imagekit.io/r2zdlyze2o/Make%20website%20Data/Temp%20SS/file_PS8h8rbjV.jpg',t:'Create engagements in HubSpot CRM from finished outgoing Yodel.io calls | Workflow Template | Make'},
+{u:'https://www.growwstacks.com/how-to-guides/2547-send-ringcentral-messages-for-new-google-forms-submissions',i:'https://ik.imagekit.io/r2zdlyze2o/Make%20website%20Data/Temp%20SS/file_MDLais5a1.jpg',t:'Send RingCentral messages for new Google Forms submissions | Workflow Template | Make'},
 {u:'https://www.growwstacks.com/how-to-guides/10036-new-template',i:'https://ik.imagekit.io/r2zdlyze2o/Make%20website%20Data/Temp%20SS/file_0u-6CgPsC.jpg',t:'Handle Verification For Twitter Webhook'},
 {u:'https://www.growwstacks.com/how-to-guides/10046-create-new-records-in-ninox-from-new-emails',i:'https://ik.imagekit.io/r2zdlyze2o/Make%20website%20Data/Temp%20SS/file_Tsqu4BKJP3.jpg',t:'Automated Whatsapp Upsell Messages For Shopify Cancellations With Rapiwa And Sheet'},
 {u:'https://www.growwstacks.com/how-to-guides/10053-create-a-softr-io-user-from-a-new-woocommerce-customer',i:'https://ik.imagekit.io/r2zdlyze2o/Make%20website%20Data/Temp%20SS/file_z4ZQJylVq.jpg?updatedAt=1773850219767',t:'Create Multiple Json Items From An Array'},
