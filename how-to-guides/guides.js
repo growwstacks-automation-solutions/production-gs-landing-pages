ce2ff294a89b141151eae44d2b48026f9c960653
faizal007-dev <faizal@growwstacks.com>
2026-03-23 18:10:25 +0530

    make templates added

--- how-to-guides/guides.js
+++ how-to-guides/guides.js
@@ -1,8 +1,3 @@
 var ALL_WORKFLOWS = [
-{u:'https://www.growwstacks.com/how-to-guides/automate_newsletters_amp_podcasts_with_ai_make',i:'https://ik.imagekit.io/r2zdlyze2o/Make%20website%20Data/How%20to%20Guided/file_JpYF7nyVv.jpg',t:'automate newsletters amp podcasts with ai make'},
-{u:'https://www.growwstacks.com/workflows/scrape-tiktok-trends-and-generate-ai-videos-with-apify-fal-ai-and-google-suite',i:'https://ik.imagekit.io/r2zdlyze2o/GS-Website/N8N-Workflows/11003-scrape-tiktok-trends-and-generate-ai-videos-with-apify.png',t:'Scrape Tiktok Trends And Generate AI Videos With Apify Fal AI And Google Suite'},
-{u:'https://www.growwstacks.com/workflows/generate-podcast-transcript-summaries-and-keywords-with-openai-and-gmail',i:'https://ik.imagekit.io/r2zdlyze2o/GS-Website/N8N-Workflows/6661-generate-podcast-transcript-summaries-and-keywords-with.png',t:'Generate Podcast Transcript Summaries And Keywords With Openai And Gmail'},
-{u:'https://www.growwstacks.com/workflows/gemini-powered-facebook-comment-and-dm-assistant-with-notion',i:'https://ik.imagekit.io/r2zdlyze2o/GS-Website/N8N-Workflows/8066-gemini-powered-facebook-comment-and-dm-assistant-with-n.png',t:'Gemini Powered Facebook Comment And Dm Assistant With Notion'},
-{u:'https://www.growwstacks.com/workflows/create-update-and-get-an-issue-on-taiga',i:'https://ik.imagekit.io/r2zdlyze2o/GS-Website/N8N-Workflows/685-create-update-and-get-an-issue-on-taiga.png',t:'Create Update And Get An Issue On Taiga'},
 
   ];
