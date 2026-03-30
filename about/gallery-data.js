34b357084e682356b3bc51b7321657b5f3a83e65
shubhampatel1010 <shubhampatel@growwstacks.com>
2026-03-30 16:39:54 +0530

    fix err

--- about/gallery-data.js
+++ about/gallery-data.js
@@ -1,6 +1,4 @@
 var GALLERY_DATA = [
-  { id: 2, type: "video", src: "https://www.youtube.com/shorts/rAVIJnZlXP0", alt: "rAVIJnZlXP0", title: "rAVIJnZlXP0", tag: "Activity" },
-  { id: 3, type: "img", src: "https://ik.imagekit.io/r2zdlyze2o/Office%20/Activity-Growwstacks/RNR5.png", alt: "RNR5", title: "RNR5", tag: "Activity" },
   {
     id: 1,
     type: "img",
