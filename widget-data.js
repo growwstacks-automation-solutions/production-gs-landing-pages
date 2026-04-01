f5c0e12c9278d9ab57f3cb60eb32fc9e695c068b
shubhampatel1010 <shubhampatel@growwstacks.com>
2026-04-01 18:09:31 +0530

    growwstacks widget update by API

---
+++ widget-data.js
@@ -0,0 +1,14 @@
+
+
+export const widgetData = [
+  {
+    "clientName": "Dr Aron",
+    "projectName": "MPP Academy",
+    "projectType": "Web",
+    "country": "US",
+    "city": "US",
+    "logoUrl": "https://v5.airtableusercontent.com/v3/u/51/51/1775052000000/X2AOJrNnq-_udSS6kwGJGw/JVqQD-BtDTdPZ4s_crSgMiyRMxw7UKLCPIqA9xeRFyxsCe47SFrry0GYew2goTx2kC0S3HkfueGbnXolWBw7cBEZpG2CrUlt6HW_rhwneCu0x-iQcXQQiee9ghB8593TSrtJY1WsW66Vi9Z4Cwm-IcsRjillTA59LCHnupmaSJe7OMTCrLGbOtz2AxK2Dajm/H9Gtwftctw9n3Zg34QUIRuggCHpAmG0H8UJ1gHQvgEs",
+    "mapimageUrl": "https://v5.airtableusercontent.com/v3/u/51/51/1775052000000/X2AOJrNnq-_udSS6kwGJGw/JVqQD-BtDTdPZ4s_crSgMiyRMxw7UKLCPIqA9xeRFyxsCe47SFrry0GYew2goTx2kC0S3HkfueGbnXolWBw7cBEZpG2CrUlt6HW_rhwneCu0x-iQcXQQiee9ghB8593TSrtJY1WsW66Vi9Z4Cwm-IcsRjillTA59LCHnupmaSJe7OMTCrLGbOtz2AxK2Dajm/H9Gtwftctw9n3Zg34QUIRuggCHpAmG0H8UJ1gHQvgEs",
+    "ProjectStatus": "Active"
+  }
+];
