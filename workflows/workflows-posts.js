226bfb2fc0db2a332f63e0f72e0c456fbbbeb16d
shubhampatel1010 <shubhampatel@growwstacks.com>
2026-04-09 15:07:50 +0530

    err fix

---
+++ workflows/workflows-posts.js
@@ -0,0 +1,3 @@
+var ALL_WORKFLOWS = [
+{u:'https://www.growwstacks.com/workflows/build-a-full-rest-api-with-n8n-webhooks',i:'https://ik.imagekit.io/r2zdlyze2o/GS-Website/N8N-Workflows/12789-build-a-full-rest-api-with-n8n-webhooks.png',t:'Build A Full Rest Api With N8N Webhooks'},
+];
