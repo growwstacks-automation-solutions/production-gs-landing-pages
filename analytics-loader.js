4f88f327f3f01c60a2c4c0c49adc0dd05cf80d86
shubhampatel1010 <shubhampatel@growwstacks.com>
2026-04-22 11:41:20 +0530

    meta pixel add

--- analytics-loader.js
+++ analytics-loader.js
@@ -23,3 +23,23 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'AW-17936939840');
 gtag('config', 'G-VDG06YNB1N');  // GA4
+
+// Meta Pixel
+!function(f,b,e,v,n,t,s){
+  if(f.fbq)return;
+  n=f.fbq=function(){
+    n.callMethod ? n.callMethod.apply(n,arguments) : n.queue.push(arguments)
+  };
+  if(!f._fbq)f._fbq=n;
+  n.push=n;
+  n.loaded=!0;
+  n.version='2.0';
+  n.queue=[];
+  t=b.createElement(e); t.async=true;
+  t.src=v;
+  s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s);
+}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+fbq('init', '1505421784623565');
+fbq('track', 'PageView');
